b1955cb00619dc0d625f664ac9c1464b7926cff0
Jackie Abert <jax.abert@gmail.com>
2020-11-06 13:26:25 -0500

    updated links to github and linkedin

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -11,8 +11,8 @@ export default class ContactPage extends Component{
                     <p>If you like what you see and are interested in hiring me for your next project, send me an email and let's schedule some time to talk. 
                     </p>
                     <ul>Be sure to check me out on:
-                    <li><a href="https://www.linkedin.com/in/jacqueline-bishop-751b151aa" target="_blank">LinkedIn</a></li>
-                    <li><a href="https://github.com/JackieBishop" target="_blank" >GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/jackie-bishop/" target="_blank">LinkedIn</a></li>
+                    <li><a href="https://github.com/Jackie-Abert" target="_blank" >GitHub</a></li>
                     <li><a href="mailto:jax.bishop.webdev@gmail.com?subject=We%20are%20interested!!">jax.bishop.webdev@gmail.com</a></li>
                         </ul>
                 </div>
