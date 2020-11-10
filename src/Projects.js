de90069dc51e295282d9db0e95c75f7bc2fc7437
Jackie Abert <jax.abert@gmail.com>
2020-11-09 22:17:43 -0500

    adding project info and links

--- src/Projects.js
+++ src/Projects.js
@@ -1,31 +1,113 @@
-import React, { Component } from 'react';
-import './CSS/Projects.css';
-import shark from './Images/shark.png';
-import eco from './Images/eco.png';
-import mountains from './Images/mountains.png';
+import React, { Component } from "react";
+import "./CSS/Projects.css";
+import fish from "./Images/fish.png";
+import rocketboy from "./Images/rocketboy.jpg";
+import battleship from "./Images/battleship.png";
 
-export default class Projects extends Component{
-    render(){
-        return(<>
+export default class Projects extends Component {
+  render() {
+    return (
+      <>
         <h2>Projects</h2>
-            <div className="project_main_container">
-                <section className="project_container">
-                    <span className="project_image image_1" aria-label="shark"></span>
-                    <label className="project_label"><h2>Label for Project Here</h2></label>
-                    {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
-                </section>
-                <section className="project_container">
-                    <span className="project_image image_2" aria-label="leaf"></span>
-                    <label className="project_label"><h2>Label for Project Here</h2></label>
-                    {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
-                </section>
-                <section className="project_container">
-                    <span className="project_image image_3" aria-label="sunset"></span>
-                    <label className="project_label"><h2>Label for Project Here</h2></label>
-                    {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
-                </section>
+        <div className="project_main_container">
+          <section className="project_container">
+            <div>
+              <img
+                src={fish}
+                alt="shark"
+                className="project_image"
+                aria-label="shark"
+              />
             </div>
-            </>
-        )
-    }
+            <label className="project_label">
+              <h2>Blackjack</h2>
+            </label>
+            <p>
+              Built for a two week solo coding challenge, this was my very
+              first, and successful, attempt at a full-stack game application.
+              It was built for future expansion of multiplayer, smarter AI,
+              achievements, and ultimately, the final goal of expanding into a
+              full poker suite
+            </p>
+            <ul>
+              Tech Stack Used
+              <li>React</li>
+              <li>JavaScript</li>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>PostgreSQL</li>
+              <li>JWT</li>
+            </ul>
+            <a target="_blank" href="https://blackjack-iokoj8agm.vercel.app/">
+              Live App
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/Jackie-Abert/jax-blackjack"
+            >
+              Client Code
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/Jackie-Abert/blackjack-server/"
+            >
+              Server Code
+            </a>
+          </section>
+          <section className="project_container">
+            <div>
+              <img
+                src={rocketboy}
+                alt="shark"
+                className="project_image"
+                aria-label="shark"
+              />
+            </div>
+            <label className="project_label">
+              <h2>Spaced Repetition</h2>
+            </label>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </section>
+          <section className="project_container">
+            <div>
+              <img
+                src={battleship}
+                alt="shark"
+                className="project_image"
+                aria-label="shark"
+              />
+            </div>
+            <label className="project_label">
+              <h2>Space Battleship</h2>
+            </label>
+            <p>
+              I was design lead for this full stack application that was a take on the
+              game Battleship. I used space elements to give new life to an old
+              concept. I also utilized elements from the main background image to
+              create a cohesive application design scheme.
+            </p>
+            <ul>
+              Tech Stack Used
+              <li>React</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>Vanilla CSS</li>
+            </ul>
+            <a target="_blank" href="https://spacebattleship.vercel.app/">
+              Live App
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/mloldacre/battleship-jmkbh"
+            >
+              Client Code
+            </a>
+          </section>
+        </div>
+      </>
+    );
+  }
 }
