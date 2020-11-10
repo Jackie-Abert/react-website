c26388871a8ea2b0bc831a2265fe517664dad5c3
Jackie Abert <jax.abert@gmail.com>
2020-11-09 22:43:06 -0500

    fixed build issues

---
+++ docs/precache-manifest.c29f2448ad5a857932ca2e886b66efe4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417188ca346f7615002d8f9eb9f7ad5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16beed19a5e26355587",
+    "url": "/static/css/main.3364691c.chunk.css"
+  },
+  {
+    "revision": "997ed9dcf571c8c6e599",
+    "url": "/static/js/2.bbde0dc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbde0dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16beed19a5e26355587",
+    "url": "/static/js/main.b21edb8d.chunk.js"
+  },
+  {
+    "revision": "f57bd6b3cd488554ebce",
+    "url": "/static/js/runtime-main.39870823.js"
+  },
+  {
+    "revision": "b3cca7ceb49ca424a704a770fc09124f",
+    "url": "/static/media/battleship.b3cca7ce.png"
+  },
+  {
+    "revision": "db724a3c0e4239a543c16c68fb2f7906",
+    "url": "/static/media/fish.db724a3c.png"
+  },
+  {
+    "revision": "eecc278b24f0cee824c6d1cf61bea5c7",
+    "url": "/static/media/jackieBishop13.eecc278b.jpg"
+  },
+  {
+    "revision": "3a8ec19e0dec537aa5a704fe4300055b",
+    "url": "/static/media/rocketboy.3a8ec19e.jpg"
+  },
+  {
+    "revision": "3312b9a31f50792e0de09fcfc3766859",
+    "url": "/static/media/technology.3312b9a3.jpg"
+  }
+]);
